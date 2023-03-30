208fc0d6a806999642fde555efb26462d92050ca
c2run <jose.cerda66@gmail.com>
2023-03-30 11:16:06 -0300

    Asignación de valor por defecto

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -4,7 +4,8 @@ import React from "react";
 export default function Saludar(props){
     //Asignación por destructuring
     const { userInfo, saludarFN} = props;
-    const { nombre } = userInfo;
+    //Asignación de valor por defecto en caso que no se asigne un valor a la constante
+    const { nombre = "Anonimo" } = userInfo;
     //saca la info de un objeto y se le asigna a una constante.
     console.log(userInfo);
     console.log(saludarFN);
