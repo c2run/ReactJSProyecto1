6fb75492916ed1b87e7cc0f05a189ba1de90bb67
c2run <jose.cerda66@gmail.com>
2023-03-30 11:12:16 -0300

    Asignación por destructuring

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -2,11 +2,16 @@ import React from "react";
 
 
 export default function Saludar(props){
-
+    //Asignación por destructuring
+    const { userInfo, saludarFN} = props;
+    const { nombre } = userInfo;
+    //saca la info de un objeto y se le asigna a una constante.
+    console.log(userInfo);
+    console.log(saludarFN);
 
     return(
         <div>
-            <button onClick={() => props.saludarFN(props.userInfo.nombre)}>Saludar</button>
+            <button onClick={() =>saludarFN(nombre)}>Saludar</button>
         </div>
     );
 }
