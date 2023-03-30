ebe18897c27f9452394c917f491abe5392d78677
c2run <jose.cerda66@gmail.com>
2023-03-30 11:30:14 -0300

    template strings

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -6,13 +6,14 @@ export default function Saludar(props){
     const { userInfo, saludarFN} = props;
     //Asignación de valor por defecto anónimo en caso que no se asigne un valor a la constante
     const { nombre = "Anonimo" } = userInfo;
+    const {edad} = userInfo;
     //saca la info de un objeto y se le asigna a una constante.
     console.log(userInfo);
     console.log(saludarFN);
 
     return(
         <div>
-            <button onClick={() =>saludarFN(nombre)}>Saludar</button>
+            <button onClick={() =>saludarFN(nombre,edad)}>Saludar</button>
         </div>
     );
 }
